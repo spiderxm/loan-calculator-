e51969c3ea8342bb72eea7445911914d884e1b4a
Mrigank Anand <53966291+spiderxm@users.noreply.github.com>
2020-01-06 14:43:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ document.getElementById('loan-form').addEventListener('submit', calculateresults
 function calculateresults(e) {
     console.log("calculating");
     e.preventDefault();
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlypayment = document.getElementById('monthly-payment');
+    const totalpayment = document.getElementById('total-payment');
+    const totalinterest = document.getElementById('total-interest');
+    
+
 
 }
