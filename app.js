bea7cea479d424480f402f4c2b137255bbc6c62a
Mrigank Anand <53966291+spiderxm@users.noreply.github.com>
2020-01-06 15:18:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,3 +29,16 @@ function calculateresults(e) {
 
 
 }
+
+function showError(error) {
+    const errorDiv = document.createElement('div');
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+    card.insertBefore(errorDiv, heading);
+    setTimeout(clearError, 3000);
+}
+function clearError(){
+    document.querySelector('.alert').remove();
+}
