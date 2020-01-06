17f61a843d8fe6e054feb2dfca409c4bd1261b4a
Mrigank Anand <53966291+spiderxm@users.noreply.github.com>
2020-01-06 15:17:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ function calculateresults(e) {
     const monthlypayment = document.getElementById('monthly-payment');
     const totalpayment = document.getElementById('total-payment');
     const totalinterest = document.getElementById('total-interest');
-    
+    const principal = parseFloat(amount.value);
+    const calculateinterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedpayments = parseFloat(years.value) * 12;
+    //monthlypayment
+    const x = Math.pow(1 + calculateinterest, calculatedpayments);
+    const monthly = (principal * x * calculateinterest) / (x - 1);
+    if (isFinite(monthly)) {
+        monthlypayment.value = monthly.toFixed(2);
+        totalpayment.value = (monthly * calculatedpayments);
+        totalinterest.value = ((monthly * calculatedpayments) - principal).toFixed(2);
+    } else {
+
+        showError('Please check your umbers');
+
+    }
+    e.preventDefault();
+
 
 
 }
